dabb7d7d2626a2449c597f06c73c6f6c7a34fe72
Peter Pan <dicken0329@gmail.com>
2021-12-27 17:51:38 +0800

    Add new file

---
+++ c_to_f.py
@@ -0,0 +1,29 @@
+#name = input('Please input name:')
+#print('Hi', name)
+
+# Good coding style
+# Adding description.
+
+# If exercise
+#rain = input ('Today have rain?')
+
+'''if rain == 'yes':
+	print('Please go out.')
+	print('Buy a video')
+	if x == 5:
+		print('I do not know why x = 5.')
+'''
+
+# If exercise 2
+# Casting 型別轉換
+'''age = input('Please input your age:')
+age = int(age)
+if age >= 20:
+	print('You are old.')
+'''
+
+# Class exercise
+# ('C) & ('F) transfer.
+degC = input('Please input temperature (degC):')
+degF = (float(degC) * 9 ) / 5 + 32
+print('Temperature is', degF, 'degF')
